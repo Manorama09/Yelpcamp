3e3004ced3c55a1beca6d9045067199b4eef8199
Manorama <manoramamaharana@gmail.com>
2020-03-28 22:20:16 +0530

    initial Commit

---
+++ v1/app.js
@@ -0,0 +1,50 @@
+var express     =require("express");
+var app         =express();
+var bodyParser  = require ("body-parser");
+var mongoose    = require("mongoose");
+var Campground  =require("./models/campground");
+var Comment     =require("./models/comment");
+var seedDB      =require("./seeds");
+var passport    =require("passport");
+var local       =require("passport-local");
+var User        =require("./models/user");
+var methodOverride=require("method-override");
+
+//Requiring routes
+var campgroundRoutes = require("./routes/campgrounds"),
+    commentRoutes    = require("./routes/comments"),
+    indexRoutes      = require("./routes/index");
+
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true })
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static(__dirname+"/public"));
+app.use(methodOverride("_method"));
+// seedDB();
+
+//PASSPORT CONFIG
+app.use(require("express-session")({
+    secret:"Secret",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new local(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+    res.locals.currentUser=req.User;
+    next();
+    });
+
+app.use(indexRoutes);
+app.use("/campgrounds/:id",commentRoutes);
+app.use("/campgrounds",campgroundRoutes);
+
+app.listen(3000,function(){
+    console.log("Server started");
+});
---
+++ v1/routes/campgrounds.js
@@ -0,0 +1,95 @@
+var express=require("express");
+var router =express.Router();
+var Campground=require("../models/campground");
+var middleware=require("../middleware");
+//INDEX
+router.get("/",function(req,res){
+    Campground.find({},function(err,allCamp){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds/campgrounds",{campgrounds:allCamp});
+    }
+});
+});
+
+//CREATE
+router.post("/", middleware.isLoggedIn, function(req,res){
+var name=req.body.name;
+var image=req.body.image;
+var desc=req.body.desc;
+
+var author={
+    id:req.user._id,
+    username:req.user.username
+};
+
+var newCamp={name:name,image:image,desc:desc,author:author}
+Campground.create( newCamp,function(err,camp)
+    {if(err){
+        console.log(err);
+    }else{
+        console.log(camp);
+        res.redirect("/campgrounds");
+    }
+});
+});
+
+//NEW
+router.get("/new",middleware.isLoggedIn, function(req,res){
+    res.render("campgrounds/new");
+});
+
+//SHOW
+router.get("/:id", function(req,res){
+    Campground.findById(req.params.id).populate("comments").exec(function(err,found){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(found);
+        res.render("campgrounds/show",{campground:found});
+    }
+    });
+});
+
+// router.post("/:id", function(req,res){
+//     Campground.findById(req.params.id).populate("comments").exec(function(err,found){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(found);
+//         res.render("campgrounds/show",{campground:found});
+//     }
+//     });
+// });
+
+//EDIT CAMPGROUND
+router.get("/:id/edit", middleware.checkOwnership, function(req, res){	
+    Campground.findById(req.params.id, function(err, foundCampground){	
+        res.render("campgrounds/edit", {campground: foundCampground});	
+    });	
+});
+
+//UPDATE CAMPGROUND 
+router.put("/:id",middleware.checkOwnership,function(req,res){
+    Campground.findByIdAndUpdate(req.params.id,req.body.campground,function(err,updatedCamp){
+        if(err){
+            res.redirect("/campgrounds");
+        }else{
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    });
+});
+
+//DESTROY CAMPGROUND
+router.delete("/:id",middleware.checkOwnership,function(req,res){
+    Campground.findByIdAndRemove(req.params.id,function(err,found){
+        if(err){
+            res.redirect("/campgrounds");
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+module.exports =router;
