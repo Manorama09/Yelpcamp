c9f4913355171152a65fd55bad2d29506681ddb2
Manorama <manoramamaharana@gmail.com>
2020-04-13 18:00:43 +0530

    dynamic price feature added

--- v1/routes/campgrounds.js
+++ v1/routes/campgrounds.js
@@ -18,13 +18,13 @@ router.post("/", middleware.isLoggedIn, function(req,res){
 var name=req.body.name;
 var image=req.body.image;
 var desc=req.body.desc;
-
+var price = req.body.price;
 var author={
     id:req.user._id,
     username:req.user.username
 };
 
-var newCamp={name:name,image:image,desc:desc,author:author}
+var newCamp={name:name,image:image,desc:desc,author:author,price:price}
 Campground.create( newCamp,function(err,camp)
     {if(err){
         console.log(err);
